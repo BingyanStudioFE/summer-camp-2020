eaf3a4ac0a07a9fe8c0484adb033c224e4d0befa
gu can <1154984091@qq.com>
2020-07-18 21:16:32 +0800

    7.18

--- G/Tasks/HTML&CSS/简书/js/homepage.js
+++ G/Tasks/HTML&CSS/简书/js/homepage.js
@@ -69,30 +69,97 @@ for (var i = 0; i < 2; i++) {
 
 // 点击展开
 var unfoldBtn = document.getElementsByClassName("unfold");
-var foldBtn = document.getElementsByClassName("fold");
 var unfoldContent = document.getElementsByClassName("passage_info_content_txt");
 var passageNumber = 0;
+var judge = 0;
+
+function judgeFold() {
+  document.getElementById("fold_unfold").innerHTML =
+    judge == 0 ? "展开" : "收回";
+}
 
 var unfold = function () {
-  unfoldContent[passageNumber].className = "pict_unfold";
-  unfoldBtn[passageNumber].className = "unfold hidden";
-  foldBtn[passageNumber].className = "fold_appear";
-};
-var fold = function () {
-  unfoldContent[passageNumber].className = "passage_info_content_txt";
-  foldBtn[passageNumber].className = "fold";
-  unfoldBtn[passageNumber].className = "unfold";
+  if (judge == 0) {
+    // judge = 1;
+    unfoldContent[passageNumber].className = "passage_info_content_txt";
+  } else {
+    // judge = 0;
+    unfoldContent[passageNumber].className =
+      "passage_info_content_txt content_fold";
+  }
 };
 
 for (var i = 0; i < unfoldContent.length; i++) {
   unfoldBtn[i].addEventListener("click", function () {
     var index = this.getAttribute("data-all");
     passageNumber = index;
+    judgeFold();
+    if (judge == 0) {
+      judge = 1;
+    } else {
+      judge = 0;
+    }
     unfold();
   });
-  foldBtn[i] = addEventListener("click", function () {
-    var index = this.getAttribute("data-brief");
-    passageNumber = index;
-    fold();
+}
+
+// 换一批、展开全部、关注
+var authors = document.getElementsByClassName("author");
+var changeBtn = document.getElementById("change");
+var allAuthorBtn = document.getElementById("allAuthor");
+var count = 10;
+var authorsNumber = new Array();
+
+// 给作者标序号
+for (var i = 0; i < count; i++) {
+  authorsNumber[i] = i;
+}
+
+// 随机排列作者序号
+function randomNumber() {
+  authorsNumber.sort(function () {
+    return 0.5 - Math.random();
+  });
+}
+
+// 清空hidden
+function clearHidden() {
+  for (var i = 0; i < count; i++) {
+    authors[authorsNumber[i]].className = "author";
+  }
+}
+
+// 数组前五个隐藏
+function changeRandom() {
+  for (var i = 0; i < 5; i++) {
+    authors[authorsNumber[i]].className = "author hidden";
+  }
+}
+
+changeRandom();
+
+// 随机换一批
+changeBtn.addEventListener("click", function () {
+  randomNumber();
+  clearHidden();
+  changeRandom();
+});
+
+// 展开全部
+allAuthorBtn.addEventListener("click", function () {
+  clearHidden();
+});
+
+// 关注
+var followBtn = document.getElementsByClassName("follow");
+var followed = new Array(10); //存放已关注的作者的序号
+var n = 0;
+
+for (var i = 0; i < count; i++) {
+  followBtn[i].addEventListener("click", function () {
+    followed[n] = this.getAttribute("data-follow");
+    n++;
+    console.log(followed[n]);
+    authors[followed[n]].className = "author hidden";
   });
 }
